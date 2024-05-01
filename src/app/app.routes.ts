2ff2dd4bcdd0af62203ff1ec8a2fc844d978d27c
MFajru <fajru234@gmail.com>
2024-05-01 12:34:10 +0700

    feat: configure routing for app and auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './shared/layouts/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes').then((x) => x.authRoutes),
+  },
+];
