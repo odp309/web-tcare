ef6ee48f922af23e307bae26d2c61a12ab282894
MFajru <fajru234@gmail.com>
2024-05-26 22:59:45 +0700

    feat: modify responsive sidebar and add BNI logo

--- src/app/admin/layouts/admin-layout/admin-layout.component.ts
+++ src/app/admin/layouts/admin-layout/admin-layout.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent, RouterOutlet],
   templateUrl: './admin-layout.component.html',
-  styleUrl: './admin-layout.component.scss'
+  styleUrl: './admin-layout.component.scss',
 })
-export class AdminLayoutComponent {
-
-}
+export class AdminLayoutComponent {}
