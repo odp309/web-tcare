5460e0f9b68f2bc05a2a03f41842da4b17b40fe6
MFajru <fajru234@gmail.com>
2024-05-29 01:22:08 +0700

    feat: add logout button

--- src/app/admin/components/sidebar/sidebar.component.ts
+++ src/app/admin/components/sidebar/sidebar.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { LucideAngularModule } from 'lucide-angular';
+import { ButtonComponent } from '../../../shared/components/button/button.component';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [LucideAngularModule, RouterLink, RouterLinkActive],
+  imports: [LucideAngularModule, RouterLink, RouterLinkActive, ButtonComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  constructor(private router: Router) {}
+
+  pathname = this.router.url;
+}
