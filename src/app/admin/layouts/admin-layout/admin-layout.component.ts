368e476bd087d1d3a699c6508dda0eaf87718634
MFajru <fajru234@gmail.com>
2024-05-29 03:03:39 +0700

    feat: add title in admin layout

--- src/app/admin/layouts/admin-layout/admin-layout.component.ts
+++ src/app/admin/layouts/admin-layout/admin-layout.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-admin-layout',
@@ -9,4 +9,13 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.scss',
 })
-export class AdminLayoutComponent {}
+export class AdminLayoutComponent implements OnInit {
+  constructor(private router: Router) {}
+  title: string = '';
+
+  ngOnInit(): void {
+    if (this.router.url.includes('dashboard')) {
+      this.title = 'Dashboard';
+    }
+  }
+}
