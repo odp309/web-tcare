524b72c3771f158837d9f740c74a63dc6d15ede8
milyanda <milyandaaa@gmail.com>
2024-05-28 13:21:52 +0700

    feat: create card component

---
+++ src/app/shared/components/card/card.component.ts
@@ -0,0 +1,14 @@
+import { NgClass } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { LucideAngularModule } from 'lucide-angular';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [NgClass],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss',
+})
+export class CardComponent {
+  @Input() customClass = '';
+}
