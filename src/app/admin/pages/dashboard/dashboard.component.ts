1db1e5964c5b4dd47508ab34a88f5bbec1713631
MFajru <fajru234@gmail.com>
2024-05-29 09:52:16 +0700

    feat: remove page header from admin layout

--- src/app/admin/pages/dashboard/dashboard.component.ts
+++ src/app/admin/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../../../shared/components/card/card.component';
 import { LucideAngularModule } from 'lucide-angular';
+import { PageHeaderComponent } from '../../components/page-header/page-header.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CardComponent, LucideAngularModule],
+  imports: [CardComponent, LucideAngularModule, PageHeaderComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
