d94708ac3cb0a0ce73a4e5b15ac70d5066b74d35
milyanda <milyandaaa@gmail.com>
2024-05-29 16:21:18 +0700

    feat: update dashboard component

--- src/app/admin/pages/dashboard/dashboard.component.ts
+++ src/app/admin/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../../../shared/components/card/card.component';
 import { LucideAngularModule } from 'lucide-angular';
+import { TableComponent } from '../../../shared/components/table/table.component';
+import { LabelStatusComponent } from '../../components/label-status/label-status.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CardComponent, LucideAngularModule],
+  imports: [
+    CardComponent,
+    LucideAngularModule,
+    TableComponent,
+    LabelStatusComponent,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
