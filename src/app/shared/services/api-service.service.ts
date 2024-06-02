71a04cc9a640491f9d09238207f0d2ba366fe004
MFajru <fajru234@gmail.com>
2024-06-02 14:57:02 +0700

    feat: update api service post headers

--- src/app/shared/services/api-service.service.ts
+++ src/app/shared/services/api-service.service.ts
@@ -2,6 +2,11 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment.development';
 
+type THeaders = {
+  'Content-Type': string;
+  Authorization?: string;
+};
+
 @Injectable({
   providedIn: 'root',
 })
@@ -13,14 +18,21 @@ export class ApiServiceService {
     body: TBody,
     token?: string
   ) {
+    let headers: THeaders = {
+      'Content-Type': 'application/json',
+    };
+    if (token) {
+      console.log('object', token);
+      headers = {
+        ...headers,
+        Authorization: `Bearer ${token}`,
+      };
+    }
     return this.http.post<TResult>(
       `${environment.apiBaseUrl}/${endpoint}`,
       body,
       {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`,
-        },
+        headers: headers,
       }
     );
   }
