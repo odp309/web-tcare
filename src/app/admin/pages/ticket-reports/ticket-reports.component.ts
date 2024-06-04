54e46fae34c13b41e07d24afe1054e2062ce9b28
MFajru <fajru234@gmail.com>
2024-06-05 04:55:37 +0700

    feat: get data from dummy db and show it

--- src/app/admin/pages/ticket-reports/ticket-reports.component.ts
+++ src/app/admin/pages/ticket-reports/ticket-reports.component.ts
@@ -9,9 +9,12 @@ import {
   TResultTicket,
 } from '../../../shared/types/ticketReport';
 import { AsyncPipe } from '@angular/common';
-import moment from 'moment';
 import { ToTitleCasePipe } from '../../../shared/pipes/to-title-case/to-title-case.pipe';
 import { FormatDatePipe } from '../../../shared/pipes/format-date/format-date.pipe';
+import { RatingsComponent } from '../../../shared/components/ratings/ratings.component';
+import { PageHeaderComponent } from '../../components/page-header/page-header.component';
+import { InputFieldComponent } from '../../../shared/components/input-field/input-field.component';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-ticket-reports',
@@ -23,12 +26,25 @@ import { FormatDatePipe } from '../../../shared/pipes/format-date/format-date.pi
     AsyncPipe,
     ToTitleCasePipe,
     FormatDatePipe,
+    RatingsComponent,
+    PageHeaderComponent,
+    InputFieldComponent,
   ],
   templateUrl: './ticket-reports.component.html',
   styleUrl: './ticket-reports.component.scss',
 })
 export class TicketReportsComponent implements OnInit {
-  constructor(private ticketService: TicketReportsService) {}
+  form!: FormGroup;
+  constructor(private ticketService: TicketReportsService, fb: FormBuilder) {
+    this.form = fb.group({
+      search: [''],
+    });
+  }
+
+  get search() {
+    return this.form.get('search');
+  }
+
   ticketReports$: Observable<ITicketReports> = EMPTY;
   ticketReporsData: TResultTicket[] = [];
   tHeads = [
@@ -61,6 +77,10 @@ export class TicketReportsComponent implements OnInit {
       isAbleToSort: false,
     },
   ];
+
+  onClick() {
+    console.log('click');
+  }
 
   ngOnInit(): void {
     this.ticketService.getTicketReports();
