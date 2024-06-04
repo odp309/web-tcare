70407ce793864ea4dcc91640a31be5771208f14f
MFajru <fajru234@gmail.com>
2024-06-04 13:50:37 +0700

    feat: fetch the data from json server

--- src/app/admin/pages/ticket-reports/ticket-reports.component.ts
+++ src/app/admin/pages/ticket-reports/ticket-reports.component.ts
@@ -1,16 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 import { LucideAngularModule } from 'lucide-angular';
 import { TableComponent } from '../../../shared/components/table/table.component';
 import { LabelStatusComponent } from '../../components/label-status/label-status.component';
+import { TicketReportsService } from '../../services/ticket-reports.service';
+import { EMPTY, Observable } from 'rxjs';
+import {
+  ITicketReports,
+  TResultTicket,
+} from '../../../shared/types/ticketReport';
+import { AsyncPipe } from '@angular/common';
+import moment from 'moment';
+import { ToTitleCasePipe } from '../../../shared/pipes/to-title-case/to-title-case.pipe';
+import { FormatDatePipe } from '../../../shared/pipes/format-date/format-date.pipe';
 
 @Component({
   selector: 'app-ticket-reports',
   standalone: true,
-  imports: [LucideAngularModule, TableComponent, LabelStatusComponent],
+  imports: [
+    LucideAngularModule,
+    TableComponent,
+    LabelStatusComponent,
+    AsyncPipe,
+    ToTitleCasePipe,
+    FormatDatePipe,
+  ],
   templateUrl: './ticket-reports.component.html',
   styleUrl: './ticket-reports.component.scss',
 })
-export class TicketReportsComponent {
+export class TicketReportsComponent implements OnInit {
+  constructor(private ticketService: TicketReportsService) {}
+  ticketReports$: Observable<ITicketReports> = EMPTY;
+  ticketReporsData: TResultTicket[] = [];
   tHeads = [
     {
       title: 'ID Tiket',
@@ -41,4 +61,12 @@ export class TicketReportsComponent {
       isAbleToSort: false,
     },
   ];
+
+  ngOnInit(): void {
+    this.ticketService.getTicketReports();
+    this.ticketReports$ = this.ticketService.getData();
+    this.ticketService.getData().subscribe((value) => {
+      this.ticketReporsData = value.result;
+    });
+  }
 }
