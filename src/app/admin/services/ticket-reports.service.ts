3e0fb51f56b9012b34bb808382a2687d9e318427
MFajru <fajru234@gmail.com>
2024-06-05 17:02:37 +0700

    feat: change data type

--- src/app/admin/services/ticket-reports.service.ts
+++ src/app/admin/services/ticket-reports.service.ts
@@ -12,10 +12,10 @@ export class TicketReportsService extends ApiServiceService {
     super(http);
   }
 
-  private data: BehaviorSubject<ITicketReports> = new BehaviorSubject(
-    {} as ITicketReports
+  private data: BehaviorSubject<TResultTicket[]> = new BehaviorSubject(
+    [] as TResultTicket[]
   );
-  private data$: Observable<ITicketReports> = this.data;
+  private data$: Observable<TResultTicket[]> = this.data;
   private isLoading: BehaviorSubject<boolean> = new BehaviorSubject(false);
   private isLoading$: Observable<boolean> = this.isLoading;
 
@@ -29,7 +29,7 @@ export class TicketReportsService extends ApiServiceService {
 
   getTicketReports() {
     this.isLoading.next(true);
-    this.get<ITicketReports>('ticket-reports')
+    this.get<TResultTicket[]>('ticket-reports-dummy')
       .pipe(
         catchError((e) => {
           this.isLoading.next(false);
@@ -44,6 +44,28 @@ export class TicketReportsService extends ApiServiceService {
           this.data.next(value);
         },
         error: (err: ITicketReports) => {
+          console.log('Something went wrong', err);
+        },
+      });
+  }
+
+  updateStatus(id: number, body: any) {
+    this.isLoading.next(true);
+    this.put<any, any>(`ticket-reports-dummy/${id}`, body)
+      .pipe(
+        catchError((e) => {
+          this.isLoading.next(false);
+          throw e.error;
+        }),
+        finalize(() => {
+          this.isLoading.next(false);
+        })
+      )
+      .subscribe({
+        next: () => {
+          this.getTicketReports();
+        },
+        error: (err: any) => {
           console.log('Something went wrong', err);
         },
       });
