fc207e9acaf2499516d6f1c26d1e81520df375da
MFajru <fajru234@gmail.com>
2024-06-06 23:13:15 +0700

    refactor: delete file

--- src/app/shared/directives/on-click-outside/on-click-outside.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { OnClickOutsideDirective } from './on-click-outside.directive';
-
-describe('OnClickOutsideDirective', () => {
-  it('should create an instance', () => {
-    const directive = new OnClickOutsideDirective();
-    expect(directive).toBeTruthy();
-  });
-});
