e6d37004dc9936cc91a3a1786eb3971834d653b3
MFajru <fajru234@gmail.com>
2024-06-07 10:25:23 +0700

    feat: add type for response and ticket report

--- src/app/shared/types/response.ts
+++ src/app/shared/types/response.ts
@@ -2,3 +2,10 @@ export interface IResponse {
   statusCode: string;
   message: string;
 }
+
+export interface IResponsePagination extends IResponse {
+  currentPage: number;
+  currentItem: number;
+  totalPage: number;
+  totalItem: number;
+}
