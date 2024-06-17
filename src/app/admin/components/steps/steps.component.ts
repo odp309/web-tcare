f15678c096a8187c5e5d39090833e8f2a0813689
MFajru <fajru234@gmail.com>
2024-06-18 00:48:47 +0700

    feat: integrate track status with API

--- src/app/admin/components/steps/steps.component.ts
+++ src/app/admin/components/steps/steps.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { IResTrackStatus } from '../../../shared/types/ticketReport';
+import { ToTitleCasePipe } from '../../../shared/pipes/to-title-case/to-title-case.pipe';
+import { FormatDatePipe } from '../../../shared/pipes/format-date/format-date.pipe';
 
 @Component({
   selector: 'app-steps',
   standalone: true,
-  imports: [],
+  imports: [ToTitleCasePipe, FormatDatePipe],
   templateUrl: './steps.component.html',
-  styleUrl: './steps.component.scss'
+  styleUrl: './steps.component.scss',
 })
-export class StepsComponent {
+export class StepsComponent implements OnChanges {
+  @Input() stepsData: IResTrackStatus[] = [];
+  @Input() numOfSteps: number = 0;
 
+  finalData: IResTrackStatus[] = [];
+
+  mappingFinalData() {
+    for (let i = 0; i < this.numOfSteps; i++) {
+      if (i <= this.stepsData.length - 1) {
+        this.finalData.push(this.stepsData[i]);
+        continue;
+      }
+      this.finalData.push(null);
+    }
+  }
+
+  ngOnChanges(): void {
+    this.mappingFinalData();
+  }
 }
