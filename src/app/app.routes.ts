cfcc82ee6e854d5a33c2ebe4c402530a223e1d8c
MFajru <fajru234@gmail.com>
2024-06-19 14:31:31 +0700

    feat: add guard in landing page, auth, and admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './shared/layouts/main-layout/main-layout.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { authGuard } from './shared/guard/auth/auth.guard';
+import { adminGuard } from './shared/guard/admin/admin.guard';
 
 export const routes: Routes = [
   {
@@ -13,17 +15,20 @@ export const routes: Routes = [
         path: '',
         component: LandingPageComponent,
         pathMatch: 'full',
+        canActivate: [authGuard],
       },
     ],
   },
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.routes').then((x) => x.authRoutes),
+    canActivate: [authGuard],
   },
   {
     path: 'admin',
     loadChildren: () =>
       import('./admin/admin.routes').then((x) => x.adminRoutes),
+    canActivate: [adminGuard],
   },
   {
     path: '**',
